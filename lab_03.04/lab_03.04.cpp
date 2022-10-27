9b40c9fdf97049bec71aa5398d8a39da22a5370b
Andriy Chykerechko Jr <andrewchiker@gmail.com>
2022-10-27 11:12:20 +0300

    commit

---
+++ lab_03.04/lab_03.04.cpp
@@ -0,0 +1,32 @@
+﻿// Lab_03_4.cpp
+// < Чикеречко Андрій >
+// Лабораторна робота № 3.4
+// Розгалуження, задане плоскою фігурою.
+// Варіант 19
+
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    double x;
+    double y;
+    double r;
+
+    cout << "x = "; cin >> x;
+    cout << "y = "; cin >> y;
+    cout << "R = "; cin >> r;
+
+    if ((y >= (-1 * x - 2 * r) && y <= 0 && x <= 0) ||
+        !(y < sqrt((r - x) * (r + x))) && x >= 0 && x <= 2 * r && y >= 0 && y <= 2 * r) {
+        cout << "yes" << endl;
+    }
+    else {
+        cout << "no" << endl;
+    }
+
+    cin.get();
+    return 0;
+}
